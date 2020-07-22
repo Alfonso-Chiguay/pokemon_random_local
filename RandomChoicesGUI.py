87284075904a743309e719a23211566b12efc8be
Alfonso-Chiguay <alfonso.chiuay@gmail.com>
2020-07-22 10:31:56 -0200

    moneda y dados

--- RandomChoicesGUI.py
+++ RandomChoicesGUI.py
@@ -13,29 +13,52 @@ class Window(Frame):
         self.generar_pkmn = Button(self)
         self.generar_pkmn["text"] = "Generar pokemon random"
         self.generar_pkmn["command"] = self.random_pkmn
-        self.generar_pkmn.place(x=80,y=20) 
+        self.generar_pkmn.place(x=40,y=20) 
 
         self.dados = Button(self)
         self.dados["text"] = "Lanzar dados"
         self.dados["command"] = self.lanzar_dados
-        self.dados.place(x=180,y=20)       
+        self.dados.place(x=480,y=20)     
+        
+        self.moneda = Button(self)
+        self.moneda["text"] = "Lanzar moneda"
+        self.moneda["command"] = self.lanzar_moneda
+        self.moneda.place(x=930,y=20)   
         
     def random_pkmn(self):        
         id_pokemon = allpkmn.random_pick()
-        print(id_pokemon)
-        load = Image.open("pkmn_img/{}.png".format(str(id_pokemon))).resize((600, 600),Image.ANTIALIAS)
+        print("Pokemon numero: "+str(id_pokemon))
+        load = Image.open("pkmn_img/{}.png".format(str(id_pokemon))).resize((400, 400),Image.ANTIALIAS)
         render = ImageTk.PhotoImage(load)
         img = Label(self, image=render)
         img.image = render
         img.place(x=10, y=70)
 
     def lanzar_dados(self):
-        numero = randint(1, 6)
+        numero = str(randint(1, 6))
+        print("Dado: "+str(numero))
+        load = Image.open("dados/{}.png".format(numero)).resize((400, 400),Image.ANTIALIAS)
+        render = ImageTk.PhotoImage(load)
+        img = Label(self, image=render)
+        img.image = render
+        img.place(x=450, y=70)
 
+    def lanzar_moneda(self):
+        numero = randint(1, 2)
+        if numero == 1:
+            moneda = "cara"
+        else:
+            moneda = "sello"
+        print("Moneda: "+moneda)
+        load = Image.open("moneda/{}.png".format(moneda)).resize((400, 400),Image.ANTIALIAS)
+        render = ImageTk.PhotoImage(load)
+        img = Label(self, image=render)
+        img.image = render
+        img.place(x=900, y=70)
 
         
 root = Tk()
 app = Window(root)
 root.wm_title("Tkinter window")
-root.geometry("1400x800")
+root.geometry("1400x600")
 root.mainloop()
