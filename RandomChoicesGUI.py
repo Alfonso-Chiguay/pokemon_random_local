3f511b95f92ef335a7c1480a4b38a599b1ea0680
Alfonso-Chiguay <alfonso.chiuay@gmail.com>
2020-07-23 10:27:22 -0200

    contadores

--- RandomChoicesGUI.py
+++ RandomChoicesGUI.py
@@ -14,16 +14,26 @@ class Window(Frame):
         self.generar_pkmn["text"] = "Generar pokemon random"
         self.generar_pkmn["command"] = self.random_pkmn
         self.generar_pkmn.place(x=40,y=20) 
+        self.contador_generar = Label(self, font=("Times New Roman","16"), fg="red")
+        self.contador_generar.place(x=200,y=20)
+        self.int_contador_generar = 0
+
 
         self.dados = Button(self)
         self.dados["text"] = "Lanzar dados"
         self.dados["command"] = self.lanzar_dados
-        self.dados.place(x=480,y=20)     
+        self.dados.place(x=480,y=20)   
+        self.contador_dados = Label(self, font=("Times New Roman","16"), fg="red")
+        self.contador_dados.place(x=600,y=20)
+        self.int_contador_dados = 0
         
         self.moneda = Button(self)
         self.moneda["text"] = "Lanzar moneda"
         self.moneda["command"] = self.lanzar_moneda
-        self.moneda.place(x=930,y=20)   
+        self.moneda.place(x=930,y=20)  
+        self.contador_moneda = Label(self, font=("Times New Roman","16"), fg="red")
+        self.contador_moneda.place(x=1100,y=20)
+        self.int_contador_moneda = 0
 
         self.lbl_nombre = Label(self, font=("Times New Roman","20"), fg="blue")
         self.lbl_tipo1 = Label(self, font=("Times New Roman","20"))
@@ -49,7 +59,8 @@ class Window(Frame):
         else:
             self.lbl_tipo1["text"] = tipos[0]
             self.lbl_tipo2["text"] = ""
-        
+        self.int_contador_generar += 1
+        self.contador_generar["text"] = str(self.int_contador_generar)
 
 
     def lanzar_dados(self):
@@ -60,6 +71,8 @@ class Window(Frame):
         img = Label(self, image=render)
         img.image = render
         img.place(x=450, y=70)
+        self.int_contador_dados += 1
+        self.contador_dados["text"] = str(self.int_contador_dados)
 
     def lanzar_moneda(self):
         numero = randint(1, 10)
@@ -73,6 +86,8 @@ class Window(Frame):
         img = Label(self, image=render)
         img.image = render
         img.place(x=900, y=70)
+        self.int_contador_moneda += 1
+        self.contador_moneda["text"] = str(self.int_contador_moneda)
 
 
 
